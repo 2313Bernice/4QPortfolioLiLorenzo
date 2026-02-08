9c27a921a28d08b13050c6629c8bd05e2a9e431b
2313Bernice <b2029bvlorenzo@pshs.edu.ph>
2026-02-08 13:24:03 +0800

    rearrange into folders

---
+++ public/q3/html.js
@@ -0,0 +1,49 @@
+
+let listString = localStorage.getItem("accounts")
+if (!listString) { accountList = {} } 
+else accountList = JSON.parse(listString) 
+
+const form = document.getElementById("dForm"); 
+
+form.addEventListener("submit", function(e) { 
+    e.preventDefault(); 
+
+    if (confirm("Submit your sign up?")) {   
+       
+        const data = new FormData(form);
+
+        const obj = Object.fromEntries(data.entries()); 
+        
+        accountList[obj.uname] = {};
+        for (let key in obj) { 
+            if (key != "uname") { 
+                accountList[obj.uname][key] = obj[key];
+            }
+        }
+        
+        console.log(accountList) 
+        listString = JSON.stringify(accountList) 
+        localStorage.setItem("accounts", listString) 
+        form.submit();
+    }
+  });
+
+
+form.addEventListener("reset", function(e) { // 
+  
+  if (!confirm("Do you wish to clear your data?")) {
+    e.preventDefault(); 
+  }
+});
+
+function inStyle(ele){
+    console.log(ele);
+    ele.style.backgroundColor = "rgb(97, 97, 182)";
+     ele.style.color="white";
+}
+
+function outStyle(ele){
+    console.log(ele);
+    ele.style.backgroundColor = "rgb(252, 239, 182)";
+    ele.style.color="black";
+}
